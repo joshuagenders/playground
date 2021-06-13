26954cfdd727bc0b8c23c737905dbfe1f7aa5022
Joshua Genders <joshuagenders@hotmail.com>
2021-06-13 17:20:13 +0100

    refactor

--- python/styles/functional.py
+++ python/styles/functional.py
@@ -4,22 +4,22 @@ from collections import deque
 from .Person import Person
 
 get_prop = lambda p : lambda item : getattr(item, p)
-curried_map = lambda fn : lambda items : map(fn, items)
-curried_reduce = lambda fn : lambda items : reduce(fn, items)
+mapper = lambda fn : lambda items : map(fn, items)
+reducer = lambda fn : lambda items : reduce(fn, items)
 
 get_salary = get_prop('Salary')
 get_name = get_prop('Name')
 
 person_to_string = lambda person: f'{get_name(person)} earns {get_salary(person)}'
-people_to_string = curried_map(person_to_string)
-print_list = curried_map(print)
+people_to_string = mapper(person_to_string)
+print_list = mapper(print)
 
-get_salaries = curried_map(get_salary)
+get_salaries = mapper(get_salary)
 sum_of = lambda a, b: a + b
-sum_list = curried_reduce(sum_of)
+sum_list = reducer(sum_of)
 calculate_total_salary = lambda people : sum_list(get_salaries(people))
 
-generate_people = curried_map(Person.get_random_person)
+generate_people = mapper(Person.get_random_person)
 
 def run_example():
     print ("== Functional ==")
